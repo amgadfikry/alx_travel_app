fd5acb11bfef134e4c91573fbc23e463665a55dc
amgadfikry <dr.amgad_sh92@yahoo.com>
2025-05-17 10:16:03 +0300

    add project

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'drf_yasg',
+    'listings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
